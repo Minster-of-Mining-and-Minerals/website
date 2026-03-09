6101787484b2ac3d1771489bddb897f93563b405
ephyg <ephywaanofii@gmail.com>
2026-03-09 16:49:53 +0300

    implement authentication and modify asm ui

--- frontend/src/providers/ReduxProvider.tsx
+++ frontend/src/providers/ReduxProvider.tsx
@@ -2,7 +2,11 @@
 
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <SessionProvider>
+            <Provider store={store}>{children}</Provider>
+        </SessionProvider>)
 }
