9a845e35adfdcae413d6aa13f2e08c13908f6d6e
AbeDevSaga <2000amandanm@gmail.com>
2026-04-19 00:26:50 +0300

    feat: add InteractiveGrid component and integrate into PageHeader

--- frontend/src/components/pages/home-page-components/PageHeader.tsx
+++ frontend/src/components/pages/home-page-components/PageHeader.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import GridBackground from '../../ui/grid-background'
+import InteractiveGrid from '../../ui/interactive-grid'
 
 const PageHeader = ({ title, icon, description }: { title: string, icon: React.ReactNode, description: string }) => {
     return (
-        <GridBackground>
+        <InteractiveGrid>
             <div className="flex mt-10 mb-10 flex-col items-center justify-center gap-4 px-4">
                 {/* Icon */}
                 <span className="font-bold border rounded-xl border-gray-500 bg-gray-500/40 text-golden-dark w-[72px] h-[72px] flex items-center justify-center text-3xl sm:text-4xl md:text-5xl">
@@ -20,7 +19,7 @@ const PageHeader = ({ title, icon, description }: { title: string, icon: React.R
                     {description}
                 </p>
             </div>
-        </GridBackground>
+        </InteractiveGrid>
     )
 }
 
