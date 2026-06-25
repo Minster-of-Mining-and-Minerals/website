6ee6303b727f3a962e443b8551dd844d9eead801
ephyg <84809084+ephyg@users.noreply.github.com>
2026-06-25 11:53:49 +0300

    Add favicon and logo icons to metadata in layout.tsx for improved branding

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -7,6 +7,13 @@ import { NextIntlClientProvider } from 'next-intl';
 export const metadata: Metadata = {
   title: "Minstry of Mines Website",
   description: "This is the official website of the Minstry of Mines",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/logo-only.png", type: "image/png", sizes: "512x512" },
+    ],
+    apple: "/logo-only.png",
+  },
 };
 
 export default function RootLayout({
